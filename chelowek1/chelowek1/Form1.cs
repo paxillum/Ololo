bb06df588652f1ea716d9c4cc2705ad12ebcc9e2
paxillum <bohdan.k.90@mail.ru>
2013-11-06 13:27:23 +0200

    fr

---
+++ chelowek1/chelowek1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace chelowek1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Form2 Form2 = new Form2();
+            Form2.Show();
+            Thread.Sleep(1000);
+            Form2.Close();
+        }
+    }
+}
---
+++ chelowek1/chelowek1/Formku2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace chelowek1
+{
+    public partial class Formku2 : Form
+    {
+        public Formku2()
+        {
+            InitializeComponent();
+        }
+    }
+}
